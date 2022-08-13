73d41da98d67c263143418ae20ec75f5349bded6
Hong <hkhong91@gmail.com>
2022-08-14 00:37:15 +0900

    first commit

---
+++ src/main/kotlin/com/example/chat/ChatServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.chat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatServerApplication
+
+fun main(args: Array<String>) {
+  runApplication<ChatServerApplication>(*args)
+}
